487cc9fd74393ab13a10ac20e9f9b1f9fd4ce8bb
Moey2Slowy <83521545+Moey2Slowy@users.noreply.github.com>
2024-01-10 20:17:12 -0600

    remove hotjar

--- header.php
+++ header.php
@@ -11,18 +11,6 @@
 	  ga('send', 'pageview');
 	
 	</script>
-	
-	<!-- Hotjar Tracking Code for www.trikoatinginc.com -->
-	<script>
-	    (function(h,o,t,j,a,r){
-	        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-	        h._hjSettings={hjid:1692210,hjsv:6};
-	        a=o.getElementsByTagName('head')[0];
-	        r=o.createElement('script');r.async=1;
-	        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-	        a.appendChild(r);
-	    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-	</script>
 
 	<title><?php wp_title() ?></title>
 	
